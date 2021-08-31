39cba0f7c675cd05edd5a256eaf0a24a77ddf487
OmarMalass <Omar_Malass_1@Outlook.com>
2021-08-31 22:54:03 +0300

    Introduced BundleMessageProcessor interface

---
+++ src/main/java/message/processor/BundleMessageProcessor.java
@@ -0,0 +1,7 @@
+package message.processor;
+
+import org.hl7.fhir.instance.model.api.IBaseBundle;
+import org.hl7.fhir.r4.model.Bundle;
+
+public interface BundleMessageProcessor extends MessageProcessor<Bundle> {
+}
